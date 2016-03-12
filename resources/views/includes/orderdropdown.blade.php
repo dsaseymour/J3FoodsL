c42da95b681c3b19501f579ed21d9ecf3d319f11
Danny <ds12rb@brocku.ca>
2016-03-12 01:03:39 -0500

    edit order dropdown

--- resources/views/includes/orderdropdown.blade.php
+++ resources/views/includes/orderdropdown.blade.php
@@ -2,86 +2,52 @@
 
 
 
-  <div class="panel panel-default">
-  <div class="panel-body">
 
     <div class="row"> <!-- start of row -->
-      <div class="col-sm-3 menu-items">
-        <a href="http://www.msn.com/en-ca/">
-          <div class="card ">
-            <img class="card-img-top center-block" src="http://placehold.it/140x100" alt="Card image caption">
-              <div class="row">
-                <div class="item-description col-sm-8">
-                  <p class="card-text"> pizza
-                  and chicken</p>
-                </div>
-                <div class="item-price col-sm-4">
-                  <p class="card-text">$50.00</p>
-                </div>
-              </div>
-        </div>
-      </a>
-      </div>
+      <div class="list-group">
 
       <div class="col-sm-3 menu-items">
-        <a href="http://www.msn.com/en-ca/">
-          <div class="card ">
-            <img class="card-img-top center-block" src="http://placehold.it/140x100" alt="Card image caption">
-              <div class="row">
-                <div class="item-description col-sm-8">
-                  <p class="card-text"> pizza
-                  and chicken</p>
-                </div>
-                <div class="item-price col-sm-4">
-                  <p class="card-text">$50.00</p>
-                </div>
-              </div>
-        </div>
-      </a>
+            <a href="#" class="list-group-item">
+              <h4 class="list-group-item-heading">Pizza and Chicken</h4>
+              <p class="list-group-item-text">
+            garlic Sauce<br />
+              supersauce</p>
+            </a>
       </div>
 
 
       <div class="col-sm-3 menu-items">
-        <a href="http://www.msn.com/en-ca/">
-          <div class="card ">
-            <img class="card-img-top center-block" src="http://placehold.it/140x100" alt="Card image caption">
-              <div class="row">
-                <div class="item-description col-sm-8">
-                  <p class="card-text"> pizza
-                  and chicken</p>
-                </div>
-                <div class="item-price col-sm-4">
-                  <p class="card-text">$50.00</p>
-                </div>
-              </div>
-        </div>
-      </a>
+            <a href="#" class="list-group-item">
+              <h4 class="list-group-item-heading">Pizza and Chicken</h4>
+              <p class="list-group-item-text">garlic Sauce<br />
+              supersauce</p>
+            </a>
       </div>
 
 
       <div class="col-sm-3 menu-items">
-        <a href="http://www.msn.com/en-ca/">
-          <div class="card ">
-            <img class="card-img-top center-block" src="http://placehold.it/140x100" alt="Card image caption">
-              <div class="row">
-                <div class="item-description col-sm-8">
-                  <p class="card-text"> pizza
-                  and chicken</p>
-                </div>
-                <div class="item-price col-sm-4">
-                  <p class="card-text">$50.00</p>
-                </div>
-              </div>
-        </div>
-      </a>
+            <a href="#" class="list-group-item">
+              <h4 class="list-group-item-heading">Pizza and Chicken</h4>
+              <p class="list-group-item-text">garlic Sauce<br />
+              supersauce</p>
+            </a>
       </div>
 
+
+      <div class="col-sm-3 menu-items">
+            <a href="#" class="list-group-item">
+              <h4 class="list-group-item-heading">Pizza and Chicken</h4>
+              <p class="list-group-item-text">garlic Sauce<br />
+              supersauce</p>
+            </a>
+      </div>
+
+
+    </div>
     </div> <!-- end of row -->
 
 
 
 
 
-  </div> <!-- panel body -->
-</div> <!-- panel -->
 </div><!-- main div -->
