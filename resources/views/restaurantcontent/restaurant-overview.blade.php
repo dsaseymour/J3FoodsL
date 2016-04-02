003eff268bfa30e1f63368c0b4761a8a8069a9ed
RileyDavidson <riley.max32@gmail.com>
2016-04-02 17:49:55 -0400

    Restaurant can now add items and categories. Currently only allows for the item price, name, and picture

--- resources/views/restaurantcontent/restaurant-overview.blade.php
+++ resources/views/restaurantcontent/restaurant-overview.blade.php
@@ -15,85 +15,85 @@ J3 Foods - Online Food Ordering
     <div class="row ">
       <div class="col-sm-12 text-center">
 
-      <div class="panel panel-default">
-        <!-- Default panel contents -->
-        <div class="panel-heading">
-          <h1>{{$restaurant->companyname}} Orders
-            <span class="badge">10</span>
-            <a class="btn btn-primary" >Close Restaurant</a>
-          </h1>
-        </div>
-        <div class="panel-body">
-                <div class="row">
-                    <div class="col-sm-12 text-center">
-                      <div class="table-responsive"><!-- Start table container -->
-                        <table class="table table-condensed table-hover table-bordered">
-                          <thead>
-                            <tr>
-                              <th>Time</th>
-                              <th>Order Info</th>
-                              <th>Pickup or Delivery</th>
-                            </tr>
-                          </thead>
-                          <tbody>
+        <div class="panel panel-default">
+          <!-- Default panel contents -->
+          <div class="panel-heading">
+            <h1>{{$restaurant->companyname}} Orders
+              <span class="badge">10</span>
+              <a class="btn btn-primary" >Close Restaurant</a>
+            </h1>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-sm-12 text-center">
+                <div class="table-responsive"><!-- Start table container -->
+                  <table class="table table-condensed table-hover table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Time</th>
+                        <th>Order Info</th>
+                        <th>Pickup or Delivery</th>
+                      </tr>
+                    </thead>
+                    <tbody>
 
-                            <tr>
-                              <td>Chicken</td>
-                              <td>
-                                <p>Mr. Jim Smith</p>
-                                <p>James Street</p>
-                                <p>St. Catharines ON</p>
-                                <p>905-378-5843</p>
-                              </td>
+                      <tr>
+                        <td>Chicken</td>
+                        <td>
+                          <p>Mr. Jim Smith</p>
+                          <p>James Street</p>
+                          <p>St. Catharines ON</p>
+                          <p>905-378-5843</p>
+                        </td>
 
-                                    <td  data-toggle="collapse" data-target="#orderdropdown">
-                                    <div data-toggle="tooltip" title="Click to show Items of Order"  >
-                                        Delivery
-                                      </div>
-                                    </td>
+                        <td  data-toggle="collapse" data-target="#orderdropdown">
+                          <div data-toggle="tooltip" title="Click to show Items of Order"  >
+                            Delivery
+                          </div>
+                        </td>
 
-                            </tr>
+                      </tr>
 
-                            <tr>
-                              <td>Chicken</td>
-                              <td>
-                                <p>Mr. Jim Smith</p>
-                                <p>James Street</p>
-                                <p>St. Catharines ON</p>
-                                <p>905-378-5843</p>
-                              </td>
-                              <td  data-toggle="collapse" data-target="#orderdropdown">
-                                  <div data-toggle="tooltip" title="Click to show Items of Order"  >
-                                  Delivery
-                                  </div>
-                              </td>
-                            </tr>
+                      <tr>
+                        <td>Chicken</td>
+                        <td>
+                          <p>Mr. Jim Smith</p>
+                          <p>James Street</p>
+                          <p>St. Catharines ON</p>
+                          <p>905-378-5843</p>
+                        </td>
+                        <td  data-toggle="collapse" data-target="#orderdropdown">
+                          <div data-toggle="tooltip" title="Click to show Items of Order"  >
+                            Delivery
+                          </div>
+                        </td>
+                      </tr>
 
-                            <tr>
-                              <td>Chicken</td>
-                              <td>
-                                <p>Mr. Jim Smith</p>
-                                <p>James Street</p>
-                                <p>St. Catharines ON</p>
-                                <p>905-378-5843</p>
-                              </td>
-                              <td  data-toggle="collapse" data-target="#orderdropdown">
-                                  <div data-toggle="tooltip" title="Click to show Items of Order"  >
-                                  Delivery
-                                  </div>
-                              </td>
-                            </tr>
+                      <tr>
+                        <td>Chicken</td>
+                        <td>
+                          <p>Mr. Jim Smith</p>
+                          <p>James Street</p>
+                          <p>St. Catharines ON</p>
+                          <p>905-378-5843</p>
+                        </td>
+                        <td  data-toggle="collapse" data-target="#orderdropdown">
+                          <div data-toggle="tooltip" title="Click to show Items of Order"  >
+                            Delivery
+                          </div>
+                        </td>
+                      </tr>
 
-                          </tbody>
-                        </table>
-                      </div><!-- End table container -->
+                    </tbody>
+                  </table>
+                </div><!-- End table container -->
 
-                    </div> <!--main column -->
-                </div><!-- row -->
-              </div> <!-- panel body -->
-      </div><!-- panel -->
-    </div><!--main column end -->
-  </div>
+              </div> <!--main column -->
+            </div><!-- row -->
+          </div> <!-- panel body -->
+        </div><!-- panel -->
+      </div><!--main column end -->
+    </div>
   </div>
 </section>
 @include('includes.orderdropdown')
@@ -102,10 +102,10 @@ J3 Foods - Online Food Ordering
 
 @section('javascript')
 <script>
-$(function() {
-  $("#restaurantnavlink-orders").addClass("active");
-  $('[data-toggle="tooltip"]').tooltip();
+  $(function() {
+    $("#restaurantnavlink-orders").addClass("active");
+    $('[data-toggle="tooltip"]').tooltip();
 
-});
+  });
 </script>
 @endsection
