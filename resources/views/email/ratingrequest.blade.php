22ae6927c10794b33b7eab419a0f9b1bcc909288
Danny <ds12rb@brocku.ca>
2016-04-02 18:17:55 -0400

    rating page

---
+++ resources/views/email/ratingrequest.blade.php
@@ -0,0 +1,11 @@
+Hi (User), we love feedback, could you take a moment to share your experience with (Restaurant)?
+
+Please select an overall rating for (Restaurant):
+
+<a href=  "{{URL::to('register/verify/' . $confirmation_code)}}"></a>5 Excellent
+4 Good
+3 Fair
+2 Poor
+1 Awful
+
+Leave a Comment:
