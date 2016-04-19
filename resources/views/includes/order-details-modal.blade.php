a52c9f5912b571a4f3aa72315c075245ea737714
anton-kurilov <anton.kurilov@outlook.com>
2016-04-19 19:09:08 -0400

    order details modal complete

--- resources/views/includes/order-details-modal.blade.php
+++ resources/views/includes/order-details-modal.blade.php
@@ -6,17 +6,23 @@
 			<td>Options</td>
 		</tr>
 		@foreach($orders as $order)
-		{{$order->item->option}}
 		<tr>
 			<td>{{$order->quantity}}</td>
 			<td>{{$order->item->name}}</td>
 			@if ($order->item->option != null)
-			<td>{{$order->item->option->name}}: 
-			{{-- */$choices=explode(',',$order->choice);/* --}}
-			@foreach ($choices as $choice)
+			<td>{{$order->item->option->name}}:
+			@if($order->item->option->type == "check")
+				{{-- */$choices=explode(',',$order->choice);/* --}}
+				@foreach ($choices as $choice)
 				{{-- */  $query = DB::table('option_choices')->where('option_id',$order->item->option->id)->where('choice_id',$choice)->first(); /* --}}
 				{{$query->name}},
-			@endforeach
+				@endforeach
+			@elseif($order->item->option->type == "combo")
+				{{-- */  $query = DB::table('option_choices')->where('option_id',$order->item->option->id)->where('choice_id',$order->choice)->first(); /* --}}
+				{{$query->name}}
+			@else
+				{{$order->choice}}
+			@endif
 			</td>
 			@else
 			<td></td>
