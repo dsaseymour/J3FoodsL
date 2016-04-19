8b821b6dc0762993aba766098690df82dc777d65
RileyDavidson <riley.max32@gmail.com>
2016-04-19 14:25:22 -0400

    trying to fix orders

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -283,12 +283,6 @@ class CustomerController extends Controller
        $user = \Auth::user();
     }
 
-   dd($user->confirmed);
-
-   /* if($user->confirmed != 1){ //if they havent confirmed their email
-      return redirect('error')->with('error-title', 'Error placing order')->with("error-message", "You have not confirmed your account, please check your email and confirm your account.");
-    }*/
-
     $orders = Orders::where('customer_id',$user)->where('completed','0')->get();
     
     foreach($orders as $items){
