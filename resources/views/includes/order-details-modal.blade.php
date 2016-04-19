9862e458c64a707ddbbb912287c8b58fc0dc42a1
anton-kurilov <anton.kurilov@outlook.com>
2016-04-19 17:51:35 -0400

    overview modal working

--- resources/views/includes/order-details-modal.blade.php
+++ resources/views/includes/order-details-modal.blade.php
@@ -6,12 +6,17 @@
 			<td>Options</td>
 		</tr>
 		@foreach($orders as $order)
+		{{$order->item->option}}
 		<tr>
 			<td>{{$order->quantity}}</td>
 			<td>{{$order->item->name}}</td>
 			@if ($order->item->option != null)
 			<td>{{$order->item->option->name}}: 
-			{{$order->choice}}
+			{{-- */$choices=explode(',',$order->choice);/* --}}
+			@foreach ($choices as $choice)
+				{{-- */  $query = DB::table('option_choices')->where('option_id',$order->item->option->id)->where('choice_id',$choice)->first(); /* --}}
+				{{$query->name}},
+			@endforeach
 			</td>
 			@else
 			<td></td>
